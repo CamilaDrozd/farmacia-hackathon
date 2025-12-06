05513a1f0101c28d6217919710922bad6933fad6
Vinicius K <80963070+NylipekWolf@users.noreply.github.com>
2025-12-06 16:22:24 -0300

    feat: atualização de estilos e routes

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './modules/login/login.component';
 import { HomeComponent } from './modules/home/home.component';
 import { CadastroComponent } from './modules/cadastro/cadastro.component';
+import { GrupoComponent } from './modules/games/grupo/grupo.component';
 
 export const routes: Routes = [
   {
@@ -16,9 +17,14 @@ export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+    children: [],
   },
   {
     path: 'cadastro',
     component: CadastroComponent,
+  },
+  {
+    path: 'grupos',
+    component: GrupoComponent,
   },
 ];
